63f6c3e13761d534ffc087dcee275a00ebdd8a86
Tiagosantos056 <tiagosantos0546@gmail.com>
2022-07-13 22:10:36 -0300

    Project created

---
+++ backend/src/main/java/com/tiagosantos056/dsmeta/DsmetaApplication.java
@@ -0,0 +1,13 @@
+package com.tiagosantos056.dsmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmetaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/tiagosantos056/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiagosantos056.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
