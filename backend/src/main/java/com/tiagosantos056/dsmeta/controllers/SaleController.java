42a6ecefe22d6eaa49881fba3d033ff7bec4d68b
Tiagosantos056 <tiagosantos0546@gmail.com>
2022-07-16 23:12:35 -0300

    Add Heroku

--- backend/src/main/java/com/tiagosantos056/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/tiagosantos056/dsmeta/controllers/SaleController.java
@@ -2,15 +2,12 @@ package com.tiagosantos056.dsmeta.controllers;
 
 import com.tiagosantos056.dsmeta.entities.Sale;
 import com.tiagosantos056.dsmeta.services.SaleService;
+import com.tiagosantos056.dsmeta.services.SmsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/sales")
@@ -19,11 +16,20 @@ public class SaleController {
     @Autowired
     private SaleService service;
 
+    @Autowired
+    private SmsService smsService;
+
+
     @GetMapping
     public Page<Sale> findSales(
             @RequestParam(value = "minDate", defaultValue = "") String minDate,
             @RequestParam(value = "maxDate", defaultValue = "") String maxDate,
             Pageable pageable){
         return service.findSales(minDate, maxDate, pageable);
+    }
+
+    @GetMapping("/{id}/notification")
+    public void notifySms(@PathVariable Long id){
+        smsService.sendSms(id);
     }
 }
--- backend/src/main/java/com/tiagosantos056/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/tiagosantos056/dsmeta/services/SaleService.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Service;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
-import java.util.List;
 
 @Service
 public class SaleService {
