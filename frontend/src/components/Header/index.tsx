8a6178091d0b225328f457a9ea89b5fb33fe8463
Tiagosantos056 <tiagosantos0546@gmail.com>
2022-07-20 21:17:48 -0300

    Add title page

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -8,12 +8,11 @@ function Header() {
         <header>
             <div className="dsmeta-logo-container">
                 <img src={logo} alt="DSMeta" />
-                <h1>DSMeta</h1>
+                <h1>DSMeta - Tiagosantos056</h1>
                 <p>
                     Desenvolvido por 
                     <a className='a' href="https://github.com/Tiagosantos056" target={'_blank'} > @Tiagosantos056  
-                    <img className='git' src= {git} alt= "GitHub" /></a>
-                    
+                    <img className='git' src= {git} alt= "GitHub" /></a> 
                 </p>
             </div>
         </header>
